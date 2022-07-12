dbcadfb23fb062c9e2a06b68ea89efe9dfb25f8e
Constantine Nathanson <constantine@cloudinary.com>
2022-07-12 08:56:54 -0400

    Update `openapi-generator.jar`

--- get_open_api_gen.sh
+++ get_open_api_gen.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # set the default value for OPEN_API_GEN_URL, or use the one that was defined before
-: "${OPEN_API_GEN_URL:=https://oss.sonatype.org/service/local/repositories/snapshots/content/com/cloudinary/openapi-generator-project/6.0.1-SNAPSHOT/openapi-generator-project-6.0.1-20220616.112442-1.jar}"
+: "${OPEN_API_GEN_URL:=https://oss.sonatype.org/service/local/repositories/snapshots/content/com/cloudinary/openapi-generator-project/6.0.2-SNAPSHOT/openapi-generator-project-6.0.2-20220712.082641-1.jar}"
 
 if ! command -v wget &> /dev/null
 then
